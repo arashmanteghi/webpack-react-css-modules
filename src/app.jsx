92932b5c5ed281f7070b109989c9e7b99fe7fee6
Arash Manteghi <arash.manteghi@gmail.com>
2016-06-17 20:28:57 +0430

    Add source code to project

---
+++ src/app.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './sass/global_scope.scss';  // import global css
+import styles from './sass/local_scope.scss'; //import local css
+
+
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			actor: 'CSS Modules ',
+		}
+	}
+	render() {
+		return (
+			<section className={styles.wrapper}>
+				<h1 className={styles.hero}>
+					<a href="https://github.com/css-modules/css-modules">
+						<span
+							className={styles.actor}
+							contentEditable="true"
+							autofocus="true"
+							dangerouslySetInnerHTML={{__html: `${this.state.actor}` }}
+						></span>
+					</a>
+					 + <a href="https://facebook.github.io/react/">React!</a>
+				</h1>
+				<h3 className={styles.lead}>Webpack–React–Babel–SCSS–PostCSS.</h3>
+			</section>
+		);
+	}
+}
+export default App;
+
+ReactDOM.render(
+	<App />,
+	document.querySelector('#app')
+);
